971ea4cc53e625a69206abbd985f1689374de4ae
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-11-15 22:23:41 -0700

    Disable user registration

--- config/fortify.php
+++ config/fortify.php
@@ -116,7 +116,7 @@ return [
     */
 
     'features' => [
-        Features::registration(),
+        // Features::registration(),
         Features::resetPasswords(),
         // Features::emailVerification(),
         Features::updateProfileInformation(),
