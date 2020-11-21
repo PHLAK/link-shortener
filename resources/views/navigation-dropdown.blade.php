9f29b336b1f4da25eb6c43c4157b1f9aaf99b300
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-11-21 15:51:26 -0700

    Added Nova and Telescope links to the dashboard

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -16,6 +16,18 @@
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ config('nova.path') }}">
+                        {{ __('Nova') }}
+                    </x-jet-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('telescope') }}">
+                        {{ __('Telescope') }}
+                    </x-jet-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
