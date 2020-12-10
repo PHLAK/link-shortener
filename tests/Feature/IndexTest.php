fe4a3e816b135fdee1e0c8e34b41c9527ef99784
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-12-09 21:53:54 -0700

    Removed example tests and added one for the index

---
+++ tests/Feature/IndexTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+/**
+ * @coversNothing
+ */
+class IndexTest extends TestCase
+{
+    public function test_it_can_access_the_index()
+    {
+        $response = $this->get('/');
+
+        $response
+            ->assertStatus(Response::HTTP_OK)
+            ->assertSee('PHLAK Links');
+    }
+}
